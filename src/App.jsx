e61921c9d9ddfdee7b3169b5c1904d7eee75b50d
ddozza54 <quokka@YJLAPTOP>
2024-02-24 22:19:22 +0900

    fetching movie data

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+    // );
+    // const json = await response.json();
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <>
+      {isLoading ? (
+        <h4>Loading...</h4>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <h2>{movie.title}</h2>
+              <img src={`${movie.medium_cover_image}`} alt='No Image' />
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App;
